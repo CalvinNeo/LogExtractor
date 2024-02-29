120c2c3cff216de5501a061d885e350590adb36a
CalvinNeo <calvinneo1995@gmail.com>
2024-03-01 00:39:20 +0800

    add joint summary
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- main.py
+++ main.py
@@ -81,23 +81,63 @@ class DSU:
     def __init__(self):
         self.fa = {}
         self.nodes = set()
+        self.prec = {}
+        self.need_reprec = True
 
     def find(self, x):
+        if (not self.need_reprec) and x in self.prec:
+            return self.prec[x]
         while x != self.fa[x]:
             x = self.fa[x]
         return x
 
     def union(self, x, y):
-        self.fa[x] = y
+        self.need_reprec = True
+        self.fa[x] = self.find(y)
 
     def add(self, x):
+        self.need_reprec = True
         if x not in self.nodes:
             self.fa[x] = x
             self.nodes.add(x)
 
+    def prehandle(self):
+        for x in self.nodes:
+            self.prec[x] = self.find(x)
+        self.need_reprec = False
+
 class Context:
     def __init__(self):
         self.digest_tree = DSU()
+
+    def joint_summary(self, a, b):
+        s1 = a.summary()
+        s2 = b.summary()
+        keys = set(list(s1.keys()) + list(s2.keys()))
+        only_in_s1 = []
+        only_in_s2 = []
+        for k in keys:
+            if k in s1 and not k in s2:
+                only_in_s1.append((k, a.digests_family[k]))
+            elif k in s2 and not k in s1:
+                only_in_s2.append((k, b.digests_family[k]))
+
+        f = open("joint.md", "w")
+        f.write("\n====== Abnormal logs on Left ======\n".format(k))
+        for (k, l) in only_in_s1:
+            f.write("{}\n".format(k))
+            f.write("```\n")
+            for x in l:
+                f.write("{}\n".format(x.replace("\n", "")))
+            f.write("```\n")
+        f.write("\n\n====== Abnormal logs on Right ======\n".format(k))
+        for (k, l) in only_in_s2:
+            f.write("{}\n".format(k))
+            f.write("```\n")
+            for x in l:
+                f.write("{}\n".format(x.replace("\n", "")))
+            f.write("```\n")
+        f.close()
 
 class LogItemExtractorOpts:
     def __init__(self):
@@ -113,7 +153,7 @@ class LogItemExtractor:
         self.extract()
 
     def extract(self):
-        elements = self.line.split(" ")
+        elements = self.line.split()
         for e in elements:
             if e.isnumeric():
                 self.features.append("")
@@ -130,8 +170,6 @@ class LogItemExtractor:
         c = dict(Counter(self.features))
         # topk = [(k, c[k]) for k in sorted(c, key = c.get, reverse = True) if k != ""]
         topk = [(k, c[k]) for k in self.features if k != ""]
-        if len(topk) == 0:
-            return ""
         n = int(len(topk) * 0.3)
         if n < self.opts.min_digest_feat:
             n = self.opts.min_digest_feat
@@ -156,6 +194,8 @@ class LogSummary:
     def digest(self):
         for f in self.features:
             d = f.digest()
+            if len(d) == 0:
+                continue
             if d in self.digests:
                 self.digests[d] += 1
                 self.digests_family[d].append(f.line)
@@ -164,13 +204,15 @@ class LogSummary:
                 self.digests[d] = 1
                 self.digests_family[d] = [f.line]
 
-        for d in self.digests:
+        for d in self.digests.keys():
             for dt in ctx.digest_tree.nodes:
                 if distance(d, dt) < min(len(d), len(dt)) * 0.5:
                     ctx.digest_tree.union(d, dt)
 
-        prepared = [(ctx.digest_tree.find(d), v) for (d, v) in self.digests.items()]
-        return {key: sum(j for i, j in group) for key, group in groupby(prepared, key=lambda x: x[0])}
+        ctx.digest_tree.prehandle()
+        # prepared = [(ctx.digest_tree.find(d), v) for (d, v) in self.digests.items()]
+        # return {key: sum(j for i, j in group) for key, group in groupby(prepared, key=lambda x: x[0])}
+        return self.digests
 
     def summary(self):
         digests = self.digest()
@@ -180,7 +222,10 @@ class LogSummary:
             if not fa in result:
                 result[fa] = []
             result[fa].extend(self.digests_family[d])
+        return result
 
+    def output_summary(self):
+        result = self.summary()
         f = open("result.md", "w")
         for fa in result:
             f.write("{}\n".format(fa))
@@ -235,13 +280,14 @@ if __name__ == '__main__':
     lines2 = f.readlines()
     f.close()
     s1 = LogSummary(ctx, lines1)
-    # s2 = LogSummary(ctx, lines2)
+    s2 = LogSummary(ctx, lines2)
     # d1 = s1.digest()
     # print(len(d1))
     # for (k, v) in d1.items():
     #     if v > 0:
     #         # print(v, "====>", k, "<=====", s1.digests_family[k].line)
     #         print(v, "====>", k)
-    s1.summary()
+    # s1.output_summary()
+    ctx.joint_summary(s1, s2)
 
 
